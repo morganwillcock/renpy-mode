5af507dc903501bbe2e299a4218582e2a4d045d8
Morgan Willcock <morgan@ice9.digital>
2025-07-08 19:20:12 +0100

    Add file header

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((emacs-lisp-mode . ((fill-column . 80)
                      (indent-tabs-mode . t))))
